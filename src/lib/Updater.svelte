7897a246931df401675a17590c2634aaa0831f35
karim-daman <38837234+karim-daman@users.noreply.github.com>
2024-08-07 21:46:06 +0100

    update

--- src/lib/Updater.svelte
+++ src/lib/Updater.svelte
@@ -4,7 +4,7 @@
   import { onMount, onDestroy } from "svelte";
   import toast from "svelte-french-toast";
 
-  let updateMessage = "!";
+  let updateMessage = "";
 
   async function handleUpdate() {
     try {
@@ -45,6 +45,11 @@
 
 <main>
   {#if updateMessage}
+    <!-- <button class=" my-1 ml-5 border rounded-sm press">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+      </svg>
+    </button> -->
     <p>{updateMessage}</p>
   {/if}
 </main>
